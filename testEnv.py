efe4a36f9ed46958beaf9431184ea3e08f9d810a
MeNoSmartBrain <ya.hemmer@gmail.com>
2022-04-26 00:11:36 +0200

    Try out read options for attacker certificates

--- testEnv.py
+++ testEnv.py
@@ -1,9 +1,28 @@
+import typing
+
 import rsa
+from os.path import exists
 
-keyPair = rsa.newkeys(16)
 
-publicKey = keyPair[0]
-privateKey = keyPair[1]
+if not exists("key"):
+    absKey = rsa.key.PublicKey(143, 3)
 
-print("P:", privateKey.p)
-print("Q:", privateKey.q)
+    out_file = open("key", "wb")
+    out_file.write(absKey._save_pkcs1_pem())
+
+    out_file.close()
+
+else:
+    in_file = open("key", "rb")
+    key = rsa.key.PublicKey._load_pkcs1_pem(in_file.read())
+
+    print(key)
+
+#
+# keyPair = rsa.newkeys(16)
+#
+# publicKey = keyPair[0]
+# privateKey = keyPair[1]
+#
+# print("P:", privateKey.p)
+# print("Q:", privateKey.q)
